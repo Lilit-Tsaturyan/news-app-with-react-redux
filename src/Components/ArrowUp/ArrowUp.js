316eefd878c2ee525067ccbc9610be29bec6b645
TUMO Labs student <tumolabsstudent@TUMOs-iMac.local>
2022-09-22 18:03:12 +0400

    just add finished project

---
+++ src/Components/ArrowUp/ArrowUp.js
@@ -0,0 +1,18 @@
+import React, { memo } from "react";
+import { BsFillArrowUpSquareFill } from "react-icons/bs";
+import "./ArrowUp.css";
+
+function ArrowUp() {
+  const goTop = () => {
+    window.scrollTo(0, 0);
+  };
+  return (
+    <div id="go-top" onClick={goTop}>
+      <p className="arrow-icon">
+        <BsFillArrowUpSquareFill />
+      </p>
+    </div>
+  );
+}
+
+export default memo(ArrowUp);
